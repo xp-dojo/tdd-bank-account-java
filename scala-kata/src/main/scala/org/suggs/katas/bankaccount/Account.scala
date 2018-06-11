e3976f92d694862b2d1c5f51af6ad137a0d581fa
Peter Suggitt <me@suggs.org.uk>
2018-06-11 07:18:41 +0100

    updated scala

--- scala-kata/src/main/scala/org/suggs/katas/bankaccount/Account.scala
+++ scala-kata/src/main/scala/org/suggs/katas/bankaccount/Account.scala
@@ -1,17 +1,7 @@
 package org.suggs.katas.bankaccount
 
-import org.suggs.katas.bankaccount.Money.anAmountOf
-
 object Account {
-  def anAccountWith(money: Money) = Account(money)
-
-  def anEmptyAccount() = Account(anAmountOf(0.0d))
-
 }
 
 case class Account(amount: Money) {
-  def withdraw(money: Money) = Account(amount.less(money))
-
-  def deposit(money: Money) = Account(amount.add(money))
-
 }
--- scala-kata/src/main/scala/org/suggs/katas/bankaccount/Money.scala
+++ scala-kata/src/main/scala/org/suggs/katas/bankaccount/Money.scala
@@ -1,12 +1,7 @@
 package org.suggs.katas.bankaccount
 
 object Money {
-  def anAmountOf(amount: Double) = new Money(amount)
 }
 
 case class Money(amount: Double) {
-
-  def less(subtraction: Money): Money = Money(amount - subtraction.amount)
-
-  def add(addition: Money): Money = Money(amount + addition.amount)
 }
