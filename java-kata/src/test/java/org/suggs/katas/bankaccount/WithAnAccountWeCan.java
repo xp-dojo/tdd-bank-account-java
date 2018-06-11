cc489212adf0fc80847308628288bf03596283b8
Peter Suggitt <me@suggs.org.uk>
2018-06-11 07:37:06 +0100

    added test log

--- java-kata/src/test/java/org/suggs/katas/bankaccount/WithAnAccountWeCan.java
+++ java-kata/src/test/java/org/suggs/katas/bankaccount/WithAnAccountWeCan.java
@@ -3,14 +3,10 @@ package org.suggs.katas.bankaccount;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.suggs.katas.bankaccount.Account.anAccountWith;
-import static org.suggs.katas.bankaccount.Account.anEmptyAccount;
-import static org.suggs.katas.bankaccount.Money.anAmountOf;
 
 public class WithAnAccountWeCan {
 
     @Test
     public void depositAnAmountToIncreaseTheBalance() {
-        assertThat(fail);
     }
 }
