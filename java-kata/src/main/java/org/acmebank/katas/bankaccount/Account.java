b73979a70491826b20429bf3d1e264e579aa52b5
Peter Suggitt <me@suggs.org.uk>
2018-06-25 07:48:19 +0100

    removed comment to keep the class immutable

--- java-kata/src/main/java/org/acmebank/katas/bankaccount/Account.java
+++ java-kata/src/main/java/org/acmebank/katas/bankaccount/Account.java
@@ -2,10 +2,6 @@ package org.acmebank.katas.bankaccount;
 
 import lombok.EqualsAndHashCode;
 
-/**
- * Immutable class to represent a bank account.
- * No accessor methods.
- */
 @EqualsAndHashCode
 public class Account {
 }
