9142ad929a4263a1b2c68b56d8a357cac83eaee3
Toby <toby.weston@gmail.com>
2019-03-03 19:34:52 +0000

    rename packages to match the second session

---
+++ src/main/java/org/xpdojo/bank/Money.java
@@ -0,0 +1,8 @@
+package org.xpdojo.bank;
+
+/**
+ * Immutable class to represent Money as a concept.
+ * This class should have no accessor methods.
+ */
+public class Money {
+}
---
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -0,0 +1,14 @@
+package org.xpdojo.bank;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class AccountTest {
+
+    @Test
+    public void depositAnAmountToIncreaseTheBalance() {
+        assertThat("your first test isn't implemented", true, is(false));
+    }
+}
