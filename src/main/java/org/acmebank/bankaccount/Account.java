d4a159e1e0a1d9459a4a912f7ef9475bbfe6e66f
Toby <toby.weston@gmail.com>
2019-03-03 19:35:37 +0000

    rename packages to match the second session

--- src/main/java/org/acmebank/bankaccount/Account.java
+++
@@ -1,4 +0,0 @@
-package org.acmebank.bankaccount;
-
-public class Account {
-}
--- src/main/java/org/acmebank/bankaccount/Money.java
+++
@@ -1,8 +0,0 @@
-package org.acmebank.bankaccount;
-
-/**
- * Immutable class to represent Money as a concept.
- * This class should have no accessor methods.
- */
-public class Money {
-}
