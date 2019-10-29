2a696e931d951a99b54157878c954791403c1d04
Toby <toby.weston@gmail.com>
2019-10-29 13:10:40 +0000

    merge from instructors branch - setup build script and travis (although there are no tests)

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,5 +1,6 @@
 package org.xpdojo.bank;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
@@ -8,6 +9,7 @@ import static org.junit.Assert.assertThat;
 public class AccountTest {
 
     @Test
+	@Ignore
     public void depositAnAmountToIncreaseTheBalance() {
         assertThat("your first test isn't implemented", true, is(false));
     }
