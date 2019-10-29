e01d07f5da9c6b8cfae2bc5c294c886ff59d17e8
Toby <toby.weston@gmail.com>
2019-10-29 10:13:59 +0000

    compile and run all tests (don't run tests if it doesn't compile)

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 mkdir -p target
-find src -type f -name "*.java" -print | xargs javac -cp "lib/*" -d target
-java -cp "lib/*:target" org.junit.runner.JUnitCore org.xpdojo.bank.AccountTest
+find src -type f -name "*.java" -print | xargs javac -cp "lib/*" -d target && java -cp "lib/*:target" org.junit.runner.JUnitCore $(find target -name "*Test.class" | tr "/" "." | sed -e "s/^target.//" -e "s/.class//")
