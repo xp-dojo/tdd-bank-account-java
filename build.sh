ced3cbfcb532e3a1a0ab73a1d025b5e807506e70
Toby <toby.weston@gmail.com>
2019-10-29 09:31:49 +0000

    run (a single) tests

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 mkdir -p target
 find src -type f -name "*.java" -print | xargs javac -cp "lib/*" -d target
+java -cp "lib/*:target" org.junit.runner.JUnitCore org.xpdojo.bank.AccountTest
