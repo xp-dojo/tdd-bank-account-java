71354fcc94e4af9424eb99b18703185da7910563
Ben Lithgow Smith <benlithgow@gmail.com>
2020-07-06 13:02:21 +0100

    use gradle for github workflow build

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p target
-find src -type f -name "*.java" -print | xargs javac -cp "lib/*" -d target && java -cp "lib/*:target" org.junit.runner.JUnitCore $(find target -name "*Test.class" | tr "/" "." | sed -e "s/^target.//" -e "s/.class//")
