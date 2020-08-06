2e6dde7c195893acf5ea2d92474de842dc40987c
wasylni <marcin@wasiluk.com>
2020-08-06 11:19:10 +0100

    balance deposit

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,7 +1,18 @@
 package org.xpdojo.bank;
 
 public class Account {
+
+    private int balanceAmount;
+
+    public Account() {
+        this.balanceAmount = 0;
+    }
+
     public int balance() {
-        return 0;
+        return balanceAmount;
+    }
+
+    public void deposit(int amount) {
+        balanceAmount = balanceAmount+amount;
     }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -3,14 +3,21 @@ package org.xpdojo.bank;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
 public class AccountTest {
 
     @Test
-	@Ignore
     public void depositAnAmountToIncreaseTheBalance() {
         Account account =new Account();
         assertEquals( 0, account.balance());
+    }
+
+    @Test
+    public void depositAmountShouldIncreaseBalance() {
+        Account account = new Account();
+        account.deposit(10);
+        assertThat(account.balance()).isEqualTo(10);
     }
 }
