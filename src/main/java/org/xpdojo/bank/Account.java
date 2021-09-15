8f4a8e23dc398dbefe05c3166adaca0012b6815b
u8avatar <90271762+u8avatar@users.noreply.github.com>
2021-09-15 13:56:14 +0200

    MY own test

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,10 @@
 package org.xpdojo.bank;
 
 public class Account {
+
+    public int balance;
+
+    public void deposit(int i) {
+        balance += i;
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -9,8 +9,29 @@ import static org.junit.Assert.assertThat;
 public class AccountTest {
 
     @Test
-	@Ignore
+	//@Ignore
     public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test isn't implemented", true, is(false));
+        //assertThat("your first test isn't implemented", true, is(false));
+        //arrange
+        Account account = new Account();
+
+        //act (add some money)
+        account.deposit(100);
+        //assert
+        assertThat(account.balance, is(100));
+    }
+
+    @Test
+    public void startingBalanceIsZero() {
+        Account account = new Account();
+        assertThat(account.balance, is(0));
+    }
+
+    @Test
+    public void depositingMultipleAmounts() {
+        Account account = new Account();
+        account.deposit(100);
+        account.deposit(300);
+        assertThat(account.balance, is(400));
     }
 }
